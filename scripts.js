7c9063d89fc353eea88317e259096d7f967642b2
naxianjim <petrasjim@yahoo.gr>
2020-03-12 11:37:20 +0200

    App is functioning, ugly

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
 function addtodo() {
-    var item = document.getElementById("input").value;
-    var ul = document.getElementById("list");
+    var item = document.getElementById("addtodos").value;
+    var ul = document.getElementById("todos");
     var li = document.createElement("li");
-   
+    li.appendChild(document.createTextNode("- " + item));
+    ul.appendChild(li);
+    document.getElementById("addtodos").value = "";
+    li.onclick = removeItem;
+}
+
+document.body.onkeyup = function(e) {
+    if (e.keyCode == 13) {
+        addtodo();
+    }
+};
+
+function removeItem(e) {
+    e.target.parentElement.removeChild(e.target);
 }
